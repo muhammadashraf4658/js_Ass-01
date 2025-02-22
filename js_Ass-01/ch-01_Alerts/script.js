80409e82a273ffbb274c6df44435f76f4351bab0
Muhammad Ashraf <164554591+muhammadashraf4658@users.noreply.github.com>
2025-02-22 18:02:56 +0500

    Add files via upload

---
+++ js_Ass-01/ch-01_Alerts/script.js
@@ -0,0 +1,26 @@
+
+// Q1 Write a script to greet your website visitor using JS alert box.
+ alert("Hey, Dear User");
+
+
+
+// Q2 Write a script to display following message on your web page:
+ alert("Error! Please Enter a Valid Password");
+
+
+// Q3 Write a script to display following message on your webpage:
+ alert("Welcome to JS Land... \nHappy Coding!");
+
+
+
+
+// Q4 Write a script to display following messages in sequence:
+ alert("Welcome to JS Land...");
+ alert("Happy Coding! ");
+
+
+// Q5 Generate the following message through browser’s developer console:
+
+
+ console.log("Hello...   I Can Run JS Through My Web Browser's Console");
+
